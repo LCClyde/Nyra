5c268448ebe56ea7831d2d0ac7d63d67fa4099fa
Clyde <clydestanfield@gmail.com>
2017-02-06 07:39:03 -0500

    Fixed some more stuff

--- scripts/install_deps_posix.sh
+++ scripts/install_deps_posix.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
 
-sudo apt-get -y install build-essential cmake libfreeimage-dev freeglut3 freeglut3-dev libglew1.5 libglew1.5-dev libglu1-mesa libglu1-mesa-dev libgl1-mesa-glx libgl1-mesa-dev libglfw3-dev libcegui-mk2-0.8.4 libxmu-dev libxi-dev subversion mercurial libarchive-dev python3-dev libois-dev libsdl2-dev libsfml-dev yasm scons libfontconfig1-dev libfreetype6-dev libpng-dev libcairo2-dev libtiff5-dev libxaw7-dev
+sudo apt-get -y install build-essential cmake freeglut3 freeglut3-dev libglew1.5 libglew1.5-dev libglu1-mesa libglu1-mesa-dev libgl1-mesa-glx libgl1-mesa-dev libglfw3-dev libxmu-dev libxi-dev subversion mercurial libarchive-dev python3-dev libois-dev libsdl2-dev libsfml-dev yasm scons libfontconfig1-dev libfreetype6-dev libpng-dev libcairo2-dev libtiff5-dev libxaw7-dev libfreeimage-dev qt5-default
 
 cd drivers
 cmake .
 make GoogleTest
 make OgreDeps
 make Ogre
-#make Boost
+make Boost
 make BoostProcess
 make sfeMovie
 make GMTL
+make CEGUI
 cd ..
 
 # Create links to CEGUI
-ln -s /usr/lib/libCEGUIBase-0.so.2.3.0 ./install/lib/libCEGUIBase-0.so
-ln -s /usr/lib/libCEGUIOpenGLRenderer-0.so.2.3.0 ./install/lib/libCEGUIOpenGLRenderer-0.so
+#ln -s /usr/lib/libCEGUIBase-0.so.2.3.0 ./install/lib/libCEGUIBase-0.so
+#ln -s /usr/lib/libCEGUIOpenGLRenderer-0.so.2.3.0 ./install/lib/libCEGUIOpenGLRenderer-0.so
 
 # We don't require anaconda right now, but probably will at some point. This
 # downlaods and installs the correct version
