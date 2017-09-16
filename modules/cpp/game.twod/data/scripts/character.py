56caa718ff734fe74b20038449ce6d99e2f6f0f1
LCClyde <clydestanfield@gmail.com>
2017-09-16 17:10:21 -0400

    Better movement

--- modules/cpp/game.twod/data/scripts/character.py
+++ modules/cpp/game.twod/data/scripts/character.py
@@ -1,5 +1,6 @@
 import nyra.game2d as nyra
 from character_grid import CharacterGrid
+from character_mover import CharacterMover
 
 class Character(nyra.Actor2D):
     def initialize(self):
@@ -11,44 +12,28 @@ class Character(nyra.Actor2D):
         starting_tile = (5, 5)
         self.transform.position = self.tilemap.tile_to_position(starting_tile)
         self.grid.reset(starting_tile)
+        self.state = 'wait'
 
     def update(self, delta):
-        vec = nyra.Vector2D()
+        if self.state == 'wait':
+            if nyra.input.is_pressed('select'):
+                self.select_tile()
+        elif self.state == 'moving':
+            self.mover.update(delta, self)
+            if self.mover.is_complete():
+                self.state  = 'wait'
+                self.grid.reset(self.tilemap.get_tile(self.transform.position))
 
-        if nyra.input.is_down("move_up"):
-            vec.y -= 1
-        if nyra.input.is_down("move_down"):
-            vec.y += 1
-        if nyra.input.is_down("move_left"):
-            vec.x -= 1
-        if nyra.input.is_down("move_right"):
-            vec.x += 1
-
-        vec.normalize()
-        self.transform.move((vec.x * delta * 100.0, vec.y * delta * 100.0))
-
-        if vec.x < 0.0:
-            self.animation = "walk_left"
-        elif vec.x > 0.0:
-            self.animation ="walk_right"
-        elif vec.y < 0.0:
-            self.animation = "walk_up"
-        elif vec.y > 0.0:
-            self.animation = "walk_down"
-        else:
-            self.animation = "idle_" + self.animation.split('_')[1]
-            
-        if nyra.input.is_pressed('select'):
-            cam_pos = self.camera.transform.position
-            cam_size = self.camera['camera'].transform.size
-            mouse_tile = self.tilemap.get_cam_tile((nyra.input.value('x'),
-                                                    nyra.input.value('y')))
-            char_tile = self.tilemap.get_tile(self.transform.position)
-            rel_tile = (mouse_tile[0] - char_tile[0],
-                        mouse_tile[1] - char_tile[1])
-            
-            if self.grid.get(rel_tile) > 0:
-                self.transform.position = self.tilemap.tile_to_position(mouse_tile)
-                self.grid.reset(mouse_tile)
-                
+    def select_tile(self):
+        cam_pos = self.camera.transform.position
+        cam_size = self.camera['camera'].transform.size
+        mouse_tile = self.tilemap.get_cam_tile((nyra.input.value('x'),
+                                                nyra.input.value('y')))
+        char_tile = self.tilemap.get_tile(self.transform.position)
+        rel_tile = (mouse_tile[0] - char_tile[0],
+                    mouse_tile[1] - char_tile[1])
         
+        if self.grid.get(rel_tile) > 0:
+            self.mover = CharacterMover([self.transform.position,
+                                        self.tilemap.tile_to_position(mouse_tile)])
+            self.state = 'moving'
---
+++ modules/cpp/game.twod/data/scripts/character_mover.py
@@ -0,0 +1,57 @@
+import nyra.game2d as nyra
+import math
+
+class MoverInfo:
+    def __init__(self, animation, direction, start, end, time):
+        self.animation = animation
+        self.direction = direction
+        self.start = start
+        self.end = end
+        self.time = time
+
+class CharacterMover:
+    def __init__(self, positions):
+        self.speed = 150
+        self.animations = []
+        time = 0
+        self.time = 0
+        for x in range(1, len(positions)):
+            direction = nyra.Vector2D(positions[x][0] - positions[x - 1][0],
+                                      positions[x][1] - positions[x - 1][1])
+            length = direction.length()
+            direction.normalize()
+            time += length / self.speed
+
+            angle = math.atan2(direction.y, direction.x)
+            name = self.angle_to_direction(math.degrees(angle))
+            entry = MoverInfo('walk_' + name, direction, positions[x - 1], positions[x], time)
+            self.animations.append(entry)
+        self.animations.append(MoverInfo('idle_down', nyra.Vector2D(), positions[-1], positions[-1], time))
+        self.index = 0
+
+    def update(self, delta, actor):
+        self.time += delta
+
+        # Keep incrementing index until we catch up to the animiation length
+        while self.index < len(self.animations) - 1 and self.animations[self.index].time < self.time:
+            actor.transform.position = self.animations[self.index].end
+            self.index += 1
+
+        anim = self.animations[self.index]
+        actor.animation = anim.animation
+        pos_x, pos_y = actor.transform.position
+        pos_x += anim.direction.x * self.speed * delta
+        pos_y += anim.direction.y * self.speed * delta
+        actor.transform.position = (pos_x, pos_y)
+        
+    def is_complete(self):
+        return self.index == len(self.animations) - 1
+
+    def angle_to_direction(self, angle):
+        if angle < -135 or angle > 135:
+            return 'left'
+        if angle < 45 and angle > -45:
+            return 'right'
+        if angle < 0:
+            return 'up'
+        return 'down'
